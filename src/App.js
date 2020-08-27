28aad2fed636f5e8656fce1ba241c4e06cbdba93
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-27 12:43:32 +0600

    navbar and courses list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Logo.png'
 import './App.css';
 
+import Navbar from './components/Navbar/Navbar.js';
+import Store from './components/Store/Store';
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar logo={logo}> </Navbar>
+      <Store></Store>
     </div>
   );
 }
---
+++ src/components/Store/Store.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Store.css';
+import Course from '../Course/Course';
+import { useState } from 'react';
+import fakeData from '../../fakeData/course.js'
+
+const Store = () => {
+    const [courses,setCourses]= useState(fakeData)
+    return (
+        <div>
+            <div className="course-container">
+            <h1 className="text-info"> Courses: {courses.length} </h1>
+            {
+                courses.map(course=><Course course={course} key={course.id}></Course>)
+            }
+            </div>
+            <div className="cart">
+                This is Cart
+            </div>
+        </div>
+    );
+};
+
+export default Store;
